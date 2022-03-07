dd5aa10d6c27851d0fcaa0d38b0381de1702f5c0
ookurayuuhei <ookura74@gmail.com>
2022-03-07 19:15:19 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,14 @@ class User < ApplicationRecord
 
 
          validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
          validates :birthday, presence: true
 
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+         validates :first_name, presence: true
+         validates :last_name, presence: true
+         end
+         with_options presence: true, format: { with: /\A[ァ-ヶ一ー]+\z/, message: '全角カタカナを使用してください' } do
+         validates :first_name_kana, presence: true
+         validates :last_name_kana, presence: true
+         end
 end
