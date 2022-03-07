27a65d9572e8cb6064e2cd1fd08b1579d8d14bec
ookurayuuhei <ookura74@gmail.com>
2022-03-07 14:36:52 +0900

    テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+  it 'emailが空では登録できない' do
+  end
+  it 'passwordが空では登録できない' do
+  end
+  it 'passwordとpassword_confirmationが不一致では登録できない' do
+  end
+  it '重複したemailが存在する場合は登録できない' do
+  end
+  it 'emailは@を含まないと登録できない' do
+  end
+  it 'passwordが5文字以下では登録できない' do
+  end
+  it 'first_nameは漢字、ひらがな以外は登録できない' do
+  end
+  it 'last_nameは漢字、ひらがな以外は登録できない' do
+  end
+  it 'first_name_kanaはカタカナ以外は登録できない' do
+  end
+  it 'last_name_kanaはカタカナ以外は登録できない' do
+  end
+  it 'birthdayが空では登録できない' do
+  end
+  # pending "add some examples to (or delete) #{__FILE__}"
+  end
 end
