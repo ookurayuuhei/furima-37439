15724eb18adf51185266133e121c4b594245baf3
ookurayuuhei <ookura74@gmail.com>
2022-03-09 23:21:50 +0900

    バリデーション設定・ActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,18 @@
 class Item < ApplicationRecord
 
+  PRICE_REGEX = /\A[0-9]+\z/.freeze
+  validates_format_of [:price], with: PRICE_REGEX, message: 'は半角数字のみ使用できます'
+
+  validates :items_name,        presence: true
+  validates :explanation,       presence: true
+  validates :category_id,       presence: true
+  validates :status_id,         presence: true
+  validates :delivery_fee_id,   presence: true
+  validates :shipping_area_id,  presence: true
+  validates :shipping_days_id,  presence: true
+  validates :price,             presence: true
+  validates :image,             presence: true
+
   belongs_to :user
   has_one_attached :image
 end
