0ddb43e4875e6bbb4f156d374d4dc5625a1b472e
ookurayuuhei <ookura74@gmail.com>
2022-03-09 17:13:22 +0900

    ActiveStorage導入２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:items_name, :explanation, :category_id,
+    :status_id, :delivery_fee_id, :shipping_area_id, :shipping_days_id,
+    :price, :image).merge(user_id: current_user.id)
+    end
 end
