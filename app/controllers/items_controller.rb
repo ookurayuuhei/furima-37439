d2f320b640f7c99bae92e169fd22006bf3f77219
ookurayuuhei <ookura74@gmail.com>
2022-03-15 01:17:02 +0900

    「もどる」詳細へ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,7 @@ class ItemsController < ApplicationController
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
-      render :
+      render :item_path
     end
   end
 
