c5659395c0ad1c840cebef19c862aa0786570848
ookurayuuhei <ookura74@gmail.com>
2022-03-15 17:34:08 +0900

    削除ボタン⇨トップページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,7 +34,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    end
   end
 
   def move_to_index
