4df4647089c7cbf3e300268447f1dbdd6c0c26f6
ookurayuuhei <ookura74@gmail.com>
2022-03-15 18:05:36 +0900

    destroy　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,8 +34,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
+    if @item.destroy
       redirect_to root_path
     end
   end
