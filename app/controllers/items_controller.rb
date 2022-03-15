d76b4ce54ee7207b863d193048b98a60528cf409
ookurayuuhei <ookura74@gmail.com>
2022-03-15 17:26:40 +0900

    destroyアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit, :update ]
-  before_action :set_item, only: [:show, :edit, :update ]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   
   def index
     @items = Item.order("created_at DESC")
@@ -31,6 +31,10 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    
   end
 
   def move_to_index
