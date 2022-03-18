87f68f7472d85bdf5ae45c288737b231a220aed6
ookurayuuhei <ookura74@gmail.com>
2022-03-19 01:08:57 +0900

    テストコード

--- spec/factories/purchase_address.rb
+++ spec/factories/purchase_address.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
-  factory :purchase_shipping_address do
-    postal_code          { '123-4567' }
-    prefecture_id        { 2 }
+  factory :purchase_address do
+    post_number          { '123-4567' }
+    shipping_area_id        { 2 }
     city                 { '東京都' }
-    addresses            { '1-1' }
-    building             { '東京ハイツ' }
-    phone_number         { '09012345678' }
+    address              { '1-1' }
+    build                { 'aaaa' }
+    phone_number         { '08012345678' }
     token                { 'tok_abcdefghijk00000000000000000' }
   end
 end
