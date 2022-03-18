bdafff48f476ff0516ba6a9bc44d27dd2a4823b2
ookurayuuhei <ookura74@gmail.com>
2022-03-18 14:17:06 +0900

    修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,16 +1,16 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
   before_action :move_to_index
+  before_action :set_item
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase__address = PurchaseAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
@@ -21,13 +21,25 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:post_number, :shipping_area_id, :city, :address, :build, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_address).permit(:post_number, :shipping_area_id, :city, :address, :build, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def move_to_index
-    item = Item.find(params[:item_id])
-    if user_signed_in? && current_user.id == item.user.id
+    if user_signed_in? && current_user.id == @item.user.id
       redirect_to root_path
     end
+end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: @purchase_shipping_address.token,
+        currency: 'jpy'
+      )
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,13 +1,14 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :post_number, :shipping_area_id, :city, :address, :build, :phone_number, :purchase_id, :user_id, :item_id
+  attr_accessor :post_number, :shipping_area_id, :city, :address, :build, :phone_number, :purchase_id, :user_id, :item_id, :token
   with_options presence: true do
-    validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :city
     validates :address
     validates :phone_number, format: {with: /[0-9]{1,11}/}
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :shipping_area_id, numericality: { other_than: 1, message: "can' be blank"}
 
